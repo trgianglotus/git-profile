ad306319ead987d6dc5a610f0fd8deb8c0055a9a
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-07-07 02:47:49 +0800

    add context for tests

--- src/test/unit/utils/formatters.js
+++ src/test/unit/utils/formatters.js
@@ -3,14 +3,15 @@ import { formatDate } from "../../../utils/formatters";
 
 const should = chai.should();
 
-describe("Date Time Formatter", function () {
-  it("should return a date time object with valid input", function () {
-    const rawText = "2016-02-11T17:27:46Z";
-    formatDate(rawText).should.equal("12-Feb-2016");
-  });
-
-  it("should return null on a bad date and time", function () {
-    const rawText = "bad input %$@";
-    should.not.exist(formatDate(rawText));
+describe("Formatters", function () {
+  describe("Date Time Formatter", function () {
+    it("should return a date time object with valid input", function () {
+      const rawText = "2016-02-11T17:27:46Z";
+      formatDate(rawText).should.equal("12-Feb-2016");
+    });
+    it("should return null on a bad date and time", function () {
+      const rawText = "bad input %$@";
+      should.not.exist(formatDate(rawText));
+    });
   });
 });
